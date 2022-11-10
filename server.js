c87a8071b178bad42cde761b9bc65a61dce18a0e
Javad Khajavi <javad_khajavi@yahoo.com>
2022-11-10 20:51:22 +0100

    Add files via upload

--- server.js
+++ server.js
@@ -1,104 +1,60 @@
 // Setup empty JS object to act as endpoint for all routes
-projectData = {
+projectData = {};
 
-};
 
 // Require Express to run server and routes
-
-const express = require('express');
+const express = require("express");
 
 // Start up an instance of app
-
 const app = express();
 
-/* Dependencies*/
-const bodyParser = require('body-parser');
 
-/* Middleware*/
-//Here we are configuring express to use body-parser as middle-ware.
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+/* ---------------Middleware---------------*/
+// CORS allows Cross-origin resource sharing policy letting client-side talk to the server
+const cors = require("cors");
 
-// Cors for cross origin allowance
-
-const cors = require('cors');
+// Enable all CORS requests
 app.use(cors());
+
+//Here we are configuring express to use body-parser as middle-ware
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: false })); // parse application/x-www-form-urlencoded
+app.use(bodyParser.json()); // parse application/json
+
 
 // Initialize the main project folder
-app.use(express.static('Website'));
+app.use(express.static("website"));
 
+/* ---------------Setup server--------------- */
+const port = 8080;
+const hostname = "127.0.0.1";
 
-// Setup Server
-
-const port = 8000;
-
-/* Spin up the server*/
-const server = app.listen(port, listening);
-
+// Testing the server 
 function listening(){
-    console.log("server running"); 
-    console.log('Running on port:' + port);
+    console.log(`Server running at http://${hostname}:${port}/`);
 }
 
-app.get('/', function (req, res) {
-    res.send('hello world');
-  });
+// spin up the server
+app.listen(port, listening);
 
-  app.post("/add", async function(req, res) {
-    const body = await req.body;
-    projectData = body;
+
+/* ---------------Communicating with the server--------------- */
+// GET
+app.get("/all", getAllData);
+
+function getAllData(req, res){
+    res.status(200).send(projectData);
+}
+
+// POST
+app.post("/add", postData);
+
+function postData(req, res){
+    projectData = req.body;
     console.log(projectData);
     res.status(200).send(projectData);
-});
-
-app.get("/all", async (req, res) => {
-    res.send(projectData);
-});
+  }
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-/*let projectData = {};
-
-const express = require('express');
-
-const app = express();
-
-const port = 8000;
-
-//parse the json files from the client side //
-app.use(express.json());
-app.use(express.urlencoded({extended: false}));
-
-app.use(express.static("Website"));
-
-const cors = require('cors');
-
-app.use(cors());
-
-app.post("/add", async function(req, res) {
-    const body = await req.body;
-    projectData = body;
-    console.log(projectData);
-    res.status(200).send(projectData);
-});
-
-app.get("/all", async (req, res) => {
-    res.send(projectData);
-});
-
-app.listen(port, function(){
-    console.log('listening on port' + port);
-});
-
-*/
